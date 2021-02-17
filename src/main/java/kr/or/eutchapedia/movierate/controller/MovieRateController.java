15002478023a9495901b247b0eebefd3305fd4d4
jeremy <jeremy96kr@naver.com>
2021-02-17 16:49:32 +0900

    PJW controll

--- src/main/java/kr/or/eutchapedia/movierate/controller/MovieRateController.java
+++ src/main/java/kr/or/eutchapedia/movierate/controller/MovieRateController.java
@@ -1,15 +1,18 @@
-package kr.or.eutchapedie.movie.rate.controller;
+package kr.or.eutchapedia.movierate.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+
+
+
+@RequestMapping("/aaaa")
 @RestController
 public class MovieRateController {
 	
-	@RequestMapping(value = "/MovieRate")
-	public ModelAndView MovieRate() {
+	@RequestMapping("/movierate")
+	public ModelAndView movieRate() {
 		ModelAndView mv = new ModelAndView("user/movie/rating/ratingPage");
-
 		return mv;
 	}
 
