80d6a0a8ac03d1137a5bc091c3c4353908fe5f49
YoonjiJE <jmjm5412@gmail.com>
2021-02-19 15:55:30 +0900

    JYJ - test

--- src/main/java/kr/or/eutchapedia/login/dao/MemberDaoInterface.java
+++ src/main/java/kr/or/eutchapedia/login/dao/MemberDaoInterface.java
@@ -2,10 +2,21 @@ package kr.or.eutchapedia.login.dao;
 
 import java.sql.SQLException;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
 import kr.or.eutchapedia.login.vo.MemberVo;
 
+@Repository
+@Mapper
 public interface MemberDaoInterface {
 	
 	//회원가입
 	int signup(MemberVo memberVo) throws SQLException;
+	
+	//이메일 중복 체크
+	int emailchk(MemberVo memberVo) throws Exception;
+	
+	//닉네임 중복 체크
+	int nicknamechk(MemberVo memberVo) throws Exception;
 }
--- src/main/java/kr/or/eutchapedia/login/service/MemberService.java
+++ src/main/java/kr/or/eutchapedia/login/service/MemberService.java
@@ -2,32 +2,52 @@ package kr.or.eutchapedia.login.service;
 
 import java.sql.SQLException;
 
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.or.eutchapedia.login.controller.Utils;
 import kr.or.eutchapedia.login.dao.MemberDaoInterface;
 import kr.or.eutchapedia.login.vo.MemberVo;
 
 @Service
 public class MemberService {
-	
+
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+	@Autowired
 	private MemberDaoInterface memberDao;
-	
-//회원가입
+	private Utils utils;
+
+	//회원가입
 	public int signup(MemberVo memberVo) {
-		
+
 		int resultCnt = 0;
-		
-		memberDao = sqlSession.getMapper(MemberDaoInterface.class);
+//		memberDao = sqlSession.getMapper(MemberDaoInterface.class);
+
 		try {
+			utils = new Utils();
+			memberVo.setMemberPwdSalt(utils.getSalt());
 			resultCnt = memberDao.signup(memberVo);
 		} catch (SQLException e) {
-			// TODO: handle exception
 			e.printStackTrace();
 		}
+		return resultCnt;
+	}
+
+	//이메일 중복 체크
+	public int emailchk(MemberVo memberVo) {
+		int resultCnt = 0;
+//		memberDao = sqlSession.getMapper(MemberDaoInterface.class);
+
+		try {
+			resultCnt = memberDao.emailchk(memberVo);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return resultCnt;
 	}
 }	
